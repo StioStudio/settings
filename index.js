5a5169d37faacc31f2ae49cfff9e72298b585027
stio <dev@stio.studio>
2024-01-05 00:06:10 +0100

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+info.language = info.language.cookie
 basicSetup()
 await lastUpdated({path: "index.html", repo: "settings"})
 let options = {
@@ -8,7 +9,7 @@ let option = document.querySelectorAll(".options")
 
 for (let i = 0; i < Object.keys(options).length; i++) {
     let rem = cookie.get(Object.keys(options)[i])
-    if(rem != "") {
+    if(rem != null) {
         options[Object.keys(options)[i]] = rem
     }
 }
@@ -17,9 +18,10 @@ option.forEach(e => {
     // console.log(e.attributes.getNamedItem("name"))
     e.value = options[e.attributes.getNamedItem("name").value]
     e.addEventListener("change", ()=>{
-        cookie.set(e.attributes.getNamedItem("name").value, e.value)
-        console.log(cookie.raw())
+        info[e.attributes.getNamedItem("name").value] = e.value
+        // cookie.set(e.attributes.getNamedItem("name").value, e.value)
+        // console.log(cookie.raw)
     })
 });
 
-await translationSetup(true)
+// await translationSetup(true)
